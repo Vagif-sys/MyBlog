f92c1e07fcd09ddfc932283547124ce712f188eb
Vagif <z.b.2021@list.ru>
2022-08-08 14:28:46 +0400

    Setup myblog main pages

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,113 @@
+@extends('main_layouts.master')
+
+@section('title', 'MyBlog | Contact')
+
+@section('content')
+<div class="colorlib-contact">
+    <div class="container">
+        <div class="row row-pb-md">
+            <div class="col-md-12 animate-box">
+                <h2>Contact Information</h2>
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="contact-info-wrap-flex">
+                            <div class="con-info">
+                                <p><span><i class="icon-location-2"></i></span> 198 West 21th Street, <br> Suite 721 New York NY 10016</p>
+                            </div>
+                            <div class="con-info">
+                                <p><span><i class="icon-phone3"></i></span> <a href="tel://1234567920">+ 1235 2355 98</a></p>
+                            </div>
+                            <div class="con-info">
+                                <p><span><i class="icon-paperplane"></i></span> <a href="mailto:info@yoursite.com">info@yoursite.com</a></p>
+                            </div>
+                            <div class="con-info">
+                                <p><span><i class="icon-globe"></i></span> <a href="#">yourwebsite.com</a></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12">
+                <h2>Message Us</h2>
+            </div>
+            <div class="col-md-6">
+                <form action="#">
+                    <div class="row form-group">
+                        <div class="col-md-6">
+                            <!-- <label for="fname">First Name</label> -->
+                            <input type="text" id="fname" class="form-control" placeholder="Your firstname">
+                        </div>
+                        <div class="col-md-6">
+                            <!-- <label for="lname">Last Name</label> -->
+                            <input type="text" id="lname" class="form-control" placeholder="Your lastname">
+                        </div>
+                    </div>
+
+                    <div class="row form-group">
+                        <div class="col-md-12">
+                            <!-- <label for="email">Email</label> -->
+                            <input type="text" id="email" class="form-control" placeholder="Your email address">
+                        </div>
+                    </div>
+
+                    <div class="row form-group">
+                        <div class="col-md-12">
+                            <!-- <label for="subject">Subject</label> -->
+                            <input type="text" id="subject" class="form-control" placeholder="Your subject of this message">
+                        </div>
+                    </div>
+
+                    <div class="row form-group">
+                        <div class="col-md-12">
+                            <!-- <label for="message">Message</label> -->
+                            <textarea name="message" id="message" cols="30" rows="10" class="form-control" placeholder="Say something about us"></textarea>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <input type="submit" value="Send Message" class="btn btn-primary">
+                    </div>
+                </form>		
+            </div>
+            <div class="col-md-6">
+                <div id="map" class="colorlib-map"></div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<div id="colorlib-subscribe" class="subs-img" style="background-image: url(images/img_bg_2.jpg);" data-stellar-background-ratio="0.5">
+    <div class="overlay"></div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2 text-center colorlib-heading animate-box">
+                <h2>Subscribe Newsletter</h2>
+                <p>Subscribe our newsletter and get latest update</p>
+            </div>
+        </div>
+        <div class="row animate-box">
+            <div class="col-md-6 col-md-offset-3">
+                <div class="row">
+                    <div class="col-md-12">
+                    <form class="form-inline qbstp-header-subscribe">
+                        <div class="col-three-forth">
+                            <div class="form-group">
+                                <input type="text" class="form-control" id="email" placeholder="Enter your email">
+                            </div>
+                        </div>
+                        <div class="col-one-third">
+                            <div class="form-group">
+                                <button type="submit" class="btn btn-primary">Subscribe Now</button>
+                            </div>
+                        </div>
+                    </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
+
